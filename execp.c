1727ac1f9959b5ee806437a62f99671c72ae0653
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2022-11-12 08:16:12 -0800

    update

--- execp.c
+++ execp.c
@@ -5,12 +5,14 @@
 
 /**
 * main - a program that calls another program
-*
+* @argc: argument count
+* @argv: argument vector
 * Return: always zero
 */
 int main(__attribute__((unused))int argc, __attribute__((unused))char **argv)
 {
 	char *arglist[3];
+
 	arglist[0] = "/usr/bin/vi";
 	arglist[1] = "split.c";
 	arglist[2] = NULL;
